b9d3f1108e6a200d81e1f9b9091079962d660aeb
Piotr Kafel <peterkaf132@gmail.com>
2023-05-04 13:36:38 +0200

    Code formated for better readibility and prepared for future data visualization

--- Data_processing.py
+++ Data_processing.py
@@ -5,77 +5,70 @@ the csv files for further operations
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
-import os
 import re
 import unidecode
 
-# Set the URL and file name for the saved HTML content
-url = "https://en.wikipedia.org/wiki/List_of_sovereign_states_and_dependent_territories_in_Europe"
-filename = "europe_states.html"
 
-# Check if the file exists
-if os.path.isfile(filename):
-    print("Found file in directory, opening it now")
-    # If the file exists, read the HTML content from the file
-    with open(filename, "r") as f:
-        html = f.read()
-else:
-    # If the file doesn't exist, make a request to the URL and save the HTML content to the file
-    print("No file found in directory, downloading one from wiki")
+def read_html_file(url, filename):
+    # Check if the file exists
+    try:
+        with open(filename, "r") as f:
+            html = f.read()
+    except FileNotFoundError:
+        # If the file doesn't exist, make a request to the URL and save the HTML content to the file
+        print("No file found in directory, downloading one from wiki")
+        html = requests.get(url).content
+        with open(filename, "wb") as f:
+            f.write(html)
+        # read from the newly created file
+        with open(filename, "r") as f:
+            html = f.read()
+    return html
 
-    html = requests.get(url).content
-    with open(filename, "wb") as f:
-        f.write(html)
 
-# Create a soup object from the HTML content
-soup = BeautifulSoup(html, 'html.parser')
+def parse_html(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    table_tags = soup.find_all('table', class_='wikitable sortable')
+    df = pd.read_html(str(table_tags))[0]
+    return df
 
-# Find the table(s) on the page and convert to a Pandas DataFrame
-table_tags = soup.find_all('table', class_='wikitable sortable')
-df = pd.read_html(str(table_tags))[0]
 
-# Set pandas display option to show all columns
-pd.set_option('display.max_columns', None)
+def normalize_dataframe(df):
+    # Drop unnecessary columns
+    df = df.drop(columns=['Flag', 'Map', 'Domestic short and formal name(s)[16][17][18]'])
 
-# Drop unnecessary columns
-df_short = df.drop(columns=['Flag', 'Map', 'Domestic short and formal name(s)[16][17][18]'])
-"""
-# Display new DataFrame
-print(df_short)
-print(df_short.columns)
-"""
+    # Rename columns
+    df = df.rename(columns={'English short, formal names, and ISO[16][17][18][19]': 'Country',
+                            'Capital[19][20][21]': 'Capital', 'Population 2021[22][23]': 'Population',
+                            'Area[a][24]': 'Area', 'Currency[19]': 'Currency'})
 
-# Rename columns
-df_short = df_short.rename(columns={'English short, formal names, and ISO[16][17][18][19]': 'Country',
-                                    'Capital[19][20][21]': 'Capital', 'Population 2021[22][23]': 'Population',
-                                    'Area[a][24]': 'Area', 'Currency[19]': 'Currency'})
-# Regex patterns
-country_pattern = r'\b[A-Z][a-z]+(?:\s[A-Z][a-z]+)*(?<!ISL)'
-capital_pattern = r'^[A-Z][a-z]+(?:\s+[A-Z][a-z]+)*(?=[^a-z]|$)'
-area_pattern = r'\d{1,3}(?:,\d{3})*(?:\.\d+)?\s+km2'
+    # Regex patterns
+    country_pattern = r'\b[A-Z][a-z]+(?:\s[A-Z][a-z]+)*(?<!ISL)'
+    capital_pattern = r'^[A-Z][a-z]+(?:\s+[A-Z][a-z]+)*(?=[^a-z]|$)'
+    area_pattern = r'\d{1,3}(?:,\d{3})*(?:\.\d+)?\s+km2'
 
-# Replace country column data with just its name
-df_short['Country'] = df_short['Country'].apply(
-    lambda x: re.search(country_pattern, x).group(0) if re.search(country_pattern, x) else None)
+    # Replace country column data with just its name
+    df['Country'] = df['Country'].apply(
+        lambda x: re.search(country_pattern, x).group(0) if re.search(country_pattern, x) else None)
 
-# Normilize Capitals for unicode
-df_short['Capital'] = df_short['Capital'].apply(
-    lambda x: unidecode.unidecode(x) if x else None)
+    # Normilize Capitals for unicode
+    df['Capital'] = df['Capital'].apply(
+        lambda x: unidecode.unidecode(x) if x else None)
 
-# Replace capital column data with just its name
-df_short['Capital'] = df_short['Capital'].apply(
-    lambda x: re.search(capital_pattern, x).group(0) if re.search(capital_pattern, x) else None)
+    # Replace capital column data with just its name
+    df['Capital'] = df['Capital'].apply(
+        lambda x: re.search(capital_pattern, x).group(0) if re.search(capital_pattern, x) else None)
 
-# Replace Area column data so it contains just km2 values
-df_short['Area'] = df_short['Area'].apply(
-    lambda x: re.search(area_pattern, x).group(0) if re.search(area_pattern, x) else None)
+    # Replace Area column data so it contains just km2 values
+    df['Area'] = df['Area'].apply(
+        lambda x: re.search(area_pattern, x).group(0) if re.search(area_pattern, x) else None)
 
-print(df_short.columns)
-print(df_short)
+    return df
 
-# Save dataframe into csv file:
-try:
-    df_short.to_csv('EU_countries.csv', index=True)
-    print('Dataframe saved to EU_countries.csv')
-except Exception as e:
-    print(f'Error saving dataframe to csv file: {str(e)}')
+
+def save_to_csv(df, filename):
+    try:
+        df.to_csv(filename, index=True)
+        print(f'Dataframe saved to {filename}')
+    except Exception as e:
+        print(f'Error saving dataframe to csv file: {str(e)}')
---
+++ main.py
@@ -0,0 +1,22 @@
+import Data_processing as dp
+
+
+def main():
+    # Set the URL and file name for the saved HTML content
+    url = "https://en.wikipedia.org/wiki/List_of_sovereign_states_and_dependent_territories_in_Europe"
+    filename = "europe_states.html"
+
+    # Parse the HTML and normalize the dataframe
+    df = dp.parse_html(dp.read_html_file(url, filename))
+    df_short = dp.normalize_dataframe(df)
+
+    # Save the dataframe to a CSV file
+    dp.save_to_csv(df_short, 'EU_countries.csv')
+
+    # Visualize the data
+    # dv.plot_population(df_short)
+    # dv.plot_area(df_short)
+
+
+if __name__ == "__main__":
+    main()
